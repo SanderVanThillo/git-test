0f18a55ef5516da104e92d986790a99bd25c881f
Sander Van Thillo <sandervanthillo@gmail.com>
2022-08-22 18:25:39 +0200

    Update README.md + Asserting that true is true + Change logging level to debug

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # git-test
 
 Testing some git features
+See what is possible with Intellij and what not
--- src/test/java/com/example/gittest/GitTestApplicationTests.java
+++ src/test/java/com/example/gittest/GitTestApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.gittest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class GitTestApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(true).isTrue();
     }
 
 }
